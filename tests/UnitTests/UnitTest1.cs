67b1a36160376e980f16d3e762bfa9fb66c7d387
Hyndavi <s533626@nwmissouri.edu>
2019-02-08 16:35:48 -0600

    last commit

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -14,13 +14,26 @@ namespace UnitTests
             var actual = Tax_Calculation(total_income,deductions);
             Assert.Equal(expected,actual);
         }
- 
- 
+        [Theory]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        public void IsEven_WorksWithManyInputs(int value)
+        {
+            Assert.True(IsEven(value));
+        }
 
-       double Tax_Calculation(int total_income,int deductions)
+
+
+        double Tax_Calculation(int total_income,int deductions)
         {
             return total_income - deductions;
         }
-       
+        bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
     }
 }
