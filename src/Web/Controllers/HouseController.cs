0002dcf5402ad10241b2191e60cfeb5dace423fd
Hyndavi <s533626@nwmissouri.edu>
2019-02-08 15:24:47 -0600

    Added methods to reply with Json, Text , Html formats added a new test of fact of function of Income_Tax_Calculation_Single() and a theory of tax_calculation().

--- src/Web/Controllers/HouseController.cs
+++ src/Web/Controllers/HouseController.cs
@@ -21,11 +21,15 @@ namespace Web.Controllers
         }
         public IActionResult Apartment()
         {
-            return View();
+           return Content("This is an Apartment, in text format");
         }
         public IActionResult Independent()
         {
-            return View();
+            return Json(new { name = "Json", description = "Welcome to Independent house in house controller, in Json format"});
+        }
+          public IActionResult Villa()
+        {
+            return Content("<html> <body> <h1> This is a Villa </h1> </body> </html>","text/html");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,21 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+       public void Income_Tax_Calculation_Single()
         {
-
+            var expected = 1900;
+            var total_income = 2000;
+            var deductions = 100;
+            var actual = Tax_Calculation(total_income,deductions);
+            Assert.Equal(expected,actual);
         }
+ 
+ 
+
+       double Tax_Calculation(int total_income,int deductions)
+        {
+            return total_income - deductions;
+        }
+       
     }
 }
